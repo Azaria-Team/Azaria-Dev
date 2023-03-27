44abcc2fbe1cda8c26120a5bdf3e24b34f15013d
Slish <90574933+Slish562@users.noreply.github.com>
2023-03-27 22:37:13 +0300

    Add files via upload

---
+++ src/hlp/HLP.java
@@ -0,0 +1,32 @@
+package hlp;
+
+import arc.Core;
+import arc.Events;
+import arc.util.Log;
+import arc.util.Time;
+import mindustry.game.EventType;
+import mindustry.mod.*;
+import galaxia.content.*;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class HLP extends Mod{
+
+    public HLP(){
+        Log.info("Loaded ExampleJavaMod constructor.");
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            Time.runTask(10f, () -> {
+
+                BaseDialog dialog = new BaseDialog("Attention!");
+                dialog.cont.add("The mod is under development and there you can see unfinished content/bugs!").row();
+
+                dialog.cont.image(Core.atlas.find("HLP-fors")).pad(20f).row();
+                dialog.cont.button("Okay", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
