e0b37ae9165a02a2ab5934178ce79f14454466d0
Slish <90574933+Slish562@users.noreply.github.com>
2023-04-22 12:49:50 +0300

    Env rework

---
+++ src/hlp/content/HLPAttribute.java
@@ -0,0 +1,9 @@
+package hlp.content;
+
+import mindustry.world.meta.Attribute;
+
+public class HLPAttribute {
+    public static Attribute
+            none = Attribute.add("none"),
+            fors = Attribute.add("fors");
+}
--- src/hlp/content/HLPBlocks.java
+++ src/hlp/content/HLPBlocks.java
@@ -2,25 +2,34 @@ package hlp.content;
 
 import mindustry.world.Block;
 import mindustry.world.blocks.environment.Floor;
+import mindustry.world.blocks.environment.OverlayFloor;
+import mindustry.world.blocks.environment.Prop;
 import mindustry.world.blocks.environment.StaticWall;
+import mindustry.world.meta.Attribute;
 
 public class HLPBlocks{
     public static Block
     //environment
-        //sw-biome
-    fir, firWall, forenite, foreniteWall, seggz, seggzWall, spaceRock, spaceRockWall,
-        //aa
-    uberraschung, uberraschungWall, serridDust, serridDustWall, fellatio, fellatioWall,
-        //liquids and each other...
-    blassenFloor;
+
+    fir, firWall, forenite, foreniteWall, forsite, forsiteWall, infernoCrystal, infernoCrystalWall,
+
+    seaSerrid, seaSerridWall, serridDust, serridDustWall, spaceRock, spaceRockWall, uberraschung, uberraschungWall,
+    //liquids and each other...
+    mainLiquid, pinkieFloor,
+
+    //prop
+    ancientSus, serridBoulder,
+
+    //ores
+    fors;
 
     public static void load() {
         fir = new Floor("fir") {{
-           variants = 4;
+            variants = 4;
         }};
 
         firWall = new StaticWall("fir-wall") {{
-
+            variants = 3;
         }};
 
         forenite = new Floor("forenite") {{
@@ -28,30 +37,31 @@ public class HLPBlocks{
         }};
 
         foreniteWall = new StaticWall("forenite-wall") {{
-
-        }};
-        seggz = new Floor("seggz") {{
             variants = 3;
         }};
 
-        seggzWall = new StaticWall("seggz-wall") {{
-
+        forsite = new Floor("forsite") {{
+            variants = 3;
         }};
 
-        spaceRock = new Floor("space-rock") {{
+        forsiteWall = new StaticWall("forsite-wall") {{
+            variants = 3;
+        }};
+
+        infernoCrystal = new Floor("inferno-crystal") {{
             variants = 4;
         }};
 
-        spaceRockWall = new StaticWall("space-rock-wall") {{
-
+        infernoCrystalWall = new StaticWall("inferno-crystal-wall") {{
+            variants = 3;
         }};
 
-        uberraschung = new Floor("uberraschung") {{
-            variants = 4;
+        seaSerrid = new Floor("sea-serrid") {{
+            variants = 3;
         }};
 
-        uberraschungWall = new StaticWall("uberraschung-wall") {{
-
+        seaSerridWall = new StaticWall("sea-serrid-wall") {{
+            variants = 3;
         }};
 
         serridDust = new Floor("serrid-dust") {{
@@ -59,19 +69,48 @@ public class HLPBlocks{
         }};
 
         serridDustWall = new StaticWall("serrid-dust-wall") {{
-
+            variants = 3;
         }};
 
-        fellatio = new Floor("fellatio") {{
+        spaceRock = new Floor("space-rock") {{
             variants = 4;
         }};
 
-        fellatioWall = new StaticWall("fellatio-wall") {{
+        spaceRockWall = new StaticWall("space-rock-wall") {{
+            variants = 3;
+        }};
 
+        uberraschung = new Floor("uberraschung") {{
+            variants = 4;
+        }};
+
+        uberraschungWall = new StaticWall("uberraschung-wall") {{
+            variants = 3;
         }};
         //LIQUIDS/ZHYZHA
-        blassenFloor = new Floor("blassen-floor") {{
+        mainLiquid = new Floor("main-liquid") {{
             variants = 4;
+            isLiquid = true;
+        }};
+
+        pinkieFloor = new Floor("pinkie-floor"){{
+           variants = 4;
+           isLiquid = true;
+        }};
+
+        //prop
+        ancientSus = new Prop("ancient-sus"){{
+            breakable = false;
+        }};
+
+        serridBoulder = new Prop("serrid-boulder"){{
+            variants = 2;
+            serridDust.asFloor().decoration = this;
+        }};
+
+        //ores
+        fors = new OverlayFloor("fors"){{
+            attributes.set(HLPAttribute.fors, 1f);
         }};
     }
 }
