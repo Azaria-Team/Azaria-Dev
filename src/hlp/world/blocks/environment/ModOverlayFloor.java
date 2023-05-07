bbb5db0d5b89050c2a075a95899bd43650ff4e23
Slish <90574933+Slish562@users.noreply.github.com>
2023-05-07 21:38:26 +0300

    ban3.1

--- src/hlp/world/blocks/environment/ModOverlayFloor.java
+++ src/hlp/world/blocks/environment/ModOverlayFloor.java
@@ -5,6 +5,8 @@ import arc.math.Mathf;
 import mindustry.world.Tile;
 import mindustry.world.blocks.environment.Floor;
 
+import static mindustry.Vars.tilesize;
+
 public class ModOverlayFloor extends Floor{
     public int variants = 2;
 
@@ -13,8 +15,11 @@ public class ModOverlayFloor extends Floor{
             useColor = false;
         }
 
-        @Override
-        public void drawBase(Tile tile){
-            Draw.rect(variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - variants))], tile.worldx(), tile.worldy());
+    @Override
+    public void drawBase(Tile tile){
+        super.drawBase(tile);
+
+            Mathf.rand.setSeed(tile.pos());
+            Draw.rect(variantRegions[Mathf.randomSeed(tile.pos(), 0, Math.max(0, variantRegions.length - 1))], tile.worldx() - tilesize, tile.worldy() - tilesize);
         }
 }
