dd84596c2e3f9cd82735e73967b38672bfbc61f2
Slish <90574933+Slish562@users.noreply.github.com>
2023-06-07 13:52:05 +0300

    ok

--- src/hlp/world/blocks/power/LightningPowerNode.java
+++ src/hlp/world/blocks/power/LightningPowerNode.java
@@ -92,15 +92,6 @@ public class LightningPowerNode extends PowerNode {
         Drawf.dashCircle(x * tilesize + offset, y * tilesize + offset, lightningRange, Pal.placing);
     }
 
-    @Override
-    public void drawLaser(float x1, float y1, float x2, float y2, int size1, int size2){
-        float angle1 = Angles.angle(x1, y1, x2, y2),
-                vx = Mathf.cosDeg(angle1), vy = Mathf.sinDeg(angle1),
-                len1 = size1 * tilesize / 2f - 1.5f, len2 = size2 * tilesize / 2f - 1.5f;
-
-        Drawf.laser(laser, laserEnd, x1 + vx*len1, y1 + vy*len1, x2 - vx*len2, y2 - vy*len2, laserScale);
-    }
-
     public class LightningPowerNodeBuild extends PowerNodeBuild {
         final Seq<LightningPowerNodeBuild> nodes = new Seq<>();
         final float[] energyToSet = new float[1];
