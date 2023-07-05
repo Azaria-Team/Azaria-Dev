d7a821bf823860fc67fd2883350789f9bdc8a0ee
xXx_M1ss1onBest_xXx <138273721+m1ss1onBest@users.noreply.github.com>
2023-07-05 06:29:35 +0300

    abandoned coast map

--- src/hpl/content/HPLBlocks.java
+++ src/hpl/content/HPLBlocks.java
@@ -435,8 +435,8 @@ public class HPLBlocks {
         }};
 
         forceTurret = new ItemTurret("force"){{
-            requirements(Category.turret, with(HPLItems.fors, 120, HPLItems.khylid, 50));
-            researchCost = with(HPLItems.fors, 200, HPLItems.khylid, 70);
+            requirements(Category.turret, with(HPLItems.fors, 70, HPLItems.khylid, 30);
+            researchCost = with(HPLItems.fors, 150, HPLItems.khylid, 70);
             health = 600;
             shootEffect = HPLFx.shootForce;
             smokeEffect = HPLFx.shootSmokeForce;
