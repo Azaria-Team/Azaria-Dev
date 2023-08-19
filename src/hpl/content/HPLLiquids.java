e03f167c313a9949393d41f500c81003cafcc2a2
m1ss1onBest <138273721+m1ss1onBest@users.noreply.github.com>
2023-08-19 18:39:21 +0300

    oxylite?

--- src/hpl/content/HPLLiquids.java
+++ src/hpl/content/HPLLiquids.java
@@ -5,11 +5,11 @@ import mindustry.type.Liquid;
 
 public class HPLLiquids {
     public static Liquid
-    mainl, pinkie;
+    oxylite, pinkie;
 
     public static void load() {
 
-        mainl = new Liquid("mainl", Color.valueOf("53ad99")) {{
+        oxylite = new Liquid("oxylite", Color.valueOf("53ad99")) {{
             viscosity = 0.65f;
             temperature = 0.3f;
         }};
