03b1a29feb1442340aa80438d06b5387f197e748
m1ss1onBest <138273721+m1ss1onBest@users.noreply.github.com>
2023-11-25 15:11:31 +0200

    add spectralia

--- src/az/content/AZLiquids.java
+++ src/az/content/AZLiquids.java
@@ -5,13 +5,18 @@ import mindustry.type.Liquid;
 
 public class AZLiquids {
     public static Liquid
-            oxyliteLiq, pinkieLiq;
+            oxyliteLiq, spectralia;
 
     public static void load() {
 
         oxyliteLiq = new Liquid("oxylite-liq", Color.valueOf("53ad99")) {{
             viscosity = 0.65f;
             heatCapacity = 0.3f;
-        }}; //test
+        }};
+
+        spectralia = new Liquid("spectralia", Color.valueOf("f4c0f4")) {{
+            viscosity = 0.65f;
+            heatCapacity = 0.3f;
+        }};
     }
 }
