bade9ae2842ecd846a9c206335e37330c1adfd18
Slish <90574933+Slish562@users.noreply.github.com>
2024-04-30 16:18:02 +0300

    some organize 2

--- src/az/graphics/AZPal.java
+++ src/az/graphics/AZPal.java
@@ -5,13 +5,8 @@ import arc.graphics.Color;
 public class AZPal {
 public static final Color
     //palette
-        aureliaOutline = Color.valueOf("2c2a36"),
-
         vogPink = Color.valueOf("ffaeb9"),
         vogPinkBack = Color.valueOf("e55690"),
-
-        droneBullet = Color.valueOf("d8f7f8"),
-        droneBulletBack = Color.valueOf("2dd5d9"),
 
         forceBullet = Color.valueOf("ffffff"),
         forceBulletBack = Color.valueOf("f27c98"),
@@ -26,18 +21,21 @@ public static final Color
         forsBack = Color.valueOf("c28392"),
         craside = Color.valueOf("d9fa96"),
         craside2 = Color.valueOf("d47f6a"),
-
-
         superdenseAlloy = Color.valueOf("5eff8f"),
-
         khylid = Color.valueOf("313442"),
 
 //units
         unmakerColor = Color.valueOf("8deb87"),
+
         zephyrLight = Color.valueOf("d8f7f8"),
         zephyrDark = Color.valueOf("2dd5d9"),
-        lightningNodeColor = Color.valueOf("f6738d"),
+
+
+        droneBullet = Color.valueOf("d8f7f8"),
+        droneBulletBack = Color.valueOf("2dd5d9"),
 //other
         forestLight = Color.valueOf("5eff8f"),
+        aureliaOutline = Color.valueOf("2c2a36"),
+        lightningNodeColor = Color.valueOf("f6738d"),
         aurionaCloud = Color.valueOf("94debc");
 }
