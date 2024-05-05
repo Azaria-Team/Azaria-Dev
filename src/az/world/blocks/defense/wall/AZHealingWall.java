82f2a1dbb9b93915ad17fbb1407d5ca732c1b3fe
m1ss1onBest <m1poisthbexxxx@gmail.com>
2024-05-05 20:24:25 +0300

    fixes

--- src/az/world/blocks/defense/wall/AZHealingWall.java
+++ src/az/world/blocks/defense/wall/AZHealingWall.java
@@ -2,8 +2,6 @@ package az.world.blocks.defense.wall;
 
 import mindustry.world.blocks.defense.Wall;
 
-import java.io.Console;
-
 public class AZHealingWall extends Wall {
     public float healAmount = 1f;
     public AZHealingWall(String name) {
