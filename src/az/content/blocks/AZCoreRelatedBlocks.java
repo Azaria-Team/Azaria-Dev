140ffbd2b4e10b9bdb7b08b1ab0fa7abfcc611e2
Slish <90574933+Slish562@users.noreply.github.com>
2024-06-18 12:50:36 +0300

    lamp radar

--- src/az/content/blocks/AZCoreRelatedBlocks.java
+++ src/az/content/blocks/AZCoreRelatedBlocks.java
@@ -1,20 +1,25 @@
 package az.content.blocks;
 
+import arc.graphics.Color;
 import az.content.AZItems;
 import az.content.AZUnits;
 import az.graphics.AZPal;
+import az.world.blocks.defense.ModRadar;
 import az.world.blocks.defense.NavalMine;
 import az.world.blocks.defense.turret.BlockRepairTurret;
+import mindustry.content.Items;
 import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.blocks.defense.Radar;
 import mindustry.world.blocks.storage.CoreBlock;
 import mindustry.world.blocks.storage.StorageBlock;
+import mindustry.world.meta.BuildVisibility;
 
 import static mindustry.type.ItemStack.with;
 
 public class AZCoreRelatedBlocks {
     public static Block
-            navalMine, coreLegion, caseI, repairTurret;
+            coreLegion, caseI, lampRadar, repairTurret, navalMine;
     public static void load() {
         //region traps
         coreLegion = new CoreBlock("core-legion") {{
@@ -40,6 +45,28 @@ public class AZCoreRelatedBlocks {
             squareSprite = false;
             researchCostMultiplier = 0.7f;
         }};
+
+        lampRadar = new ModRadar("lamp-radar"){{
+            requirements(Category.effect, BuildVisibility.fogOnly, with(AZItems.fors, 50, AZItems.lepera, 20));
+            outlineColor = Color.valueOf("4a4b53");
+            fogRadius = 34;
+            researchCost = with(AZItems.fors, 40, AZItems.lepera, 20);
+
+            consumePower(0.3f);
+        }};
+
+        repairTurret = new BlockRepairTurret("repair-turret"){{
+            requirements(Category.effect, with(AZItems.fors, 90, AZItems.lepera, 40));
+            health = 320;
+            repairSpeed = 0.75f;
+            repairRadius = 90f;
+            outlineColor = AZPal.aureliaOutline;
+            beamWidth = 1f;
+            powerUse = 0.7f;
+            size= 2;
+            researchCostMultiplier = 0.5f;
+        }};
+
         navalMine = new NavalMine("naval-mine") {{
             size = 2;
             placeableLiquid = true;
@@ -50,18 +77,6 @@ public class AZCoreRelatedBlocks {
             health = 1850;
             damage = 790;
             tileDamage = 1850;
-        }};
-
-        repairTurret = new BlockRepairTurret("repair-turret"){{
-            requirements(Category.effect, with(AZItems.fors, 90, AZItems.lepera, 40));
-            health = 320;
-            repairSpeed = 0.75f;
-            repairRadius = 90f;
-            outlineColor = AZPal.aureliaOutline;
-            beamWidth = 1f;
-            powerUse = 1f;
-            size= 2;
-            researchCostMultiplier = 0.5f;
         }};
     }
 }
---
+++ src/az/world/blocks/defense/ModRadar.java
@@ -0,0 +1,95 @@
+package az.world.blocks.defense;
+
+import arc.graphics.Color;
+import arc.graphics.g2d.Draw;
+import arc.graphics.g2d.TextureRegion;
+import arc.math.Mathf;
+import arc.struct.EnumSet;
+import arc.util.io.Reads;
+import arc.util.io.Writes;
+import mindustry.Vars;
+import mindustry.gen.Building;
+import mindustry.graphics.Drawf;
+import mindustry.graphics.Layer;
+import mindustry.graphics.Pal;
+import mindustry.world.Block;
+import mindustry.world.blocks.defense.Radar;
+import mindustry.world.meta.BlockFlag;
+
+import static mindustry.Vars.tilesize;
+
+public class ModRadar extends Block {
+    public float discoveryTime = 60f * 20f;
+
+    public ModRadar(String name){
+        super(name);
+
+        update = solid = true;
+        flags = EnumSet.of(BlockFlag.hasFogRadius);
+        outlineIcon = true;
+        fogRadius = 20;
+    }
+
+    @Override
+    public void drawPlace(int x, int y, int rotation, boolean valid){
+        super.drawPlace(x, y, rotation, valid);
+
+        Drawf.dashCircle(x * tilesize + offset, y * tilesize + offset, fogRadius * tilesize, Pal.accent);
+    }
+
+    public class RadarBuild extends Building {
+        public float progress;
+        public float lastRadius = 0f;
+        public float smoothEfficiency = 1f;
+        public float totalProgress;
+
+        @Override
+        public float fogRadius(){
+            return fogRadius * progress * smoothEfficiency;
+        }
+
+        @Override
+        public void updateTile(){
+            smoothEfficiency = Mathf.lerpDelta(smoothEfficiency, efficiency, 0.05f);
+
+            if(Math.abs(fogRadius() - lastRadius) >= 0.5f){
+                Vars.fogControl.forceUpdate(team, this);
+                lastRadius = fogRadius();
+            }
+
+            progress += edelta() / discoveryTime;
+            progress = Mathf.clamp(progress);
+
+            totalProgress += efficiency * edelta();
+        }
+
+        @Override
+        public boolean canPickup(){
+            return false;
+        }
+
+        @Override
+        public void drawSelect(){
+            Drawf.dashCircle(x, y, fogRadius() * tilesize, Pal.accent);
+        }
+
+        @Override
+        public float progress(){
+            return progress;
+        }
+
+        @Override
+        public void write(Writes write){
+            super.write(write);
+
+            write.f(progress);
+        }
+
+        @Override
+        public void read(Reads read, byte revision){
+            super.read(read, revision);
+
+            progress = read.f();
+        }
+    }
+}
