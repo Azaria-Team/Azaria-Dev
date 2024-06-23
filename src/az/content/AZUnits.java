257f3ee68a97b6466460261d35b30277affdd070
Slish <90574933+Slish562@users.noreply.github.com>
2024-06-23 13:52:23 +0300

    sentinel

--- src/az/content/AZUnits.java
+++ src/az/content/AZUnits.java
@@ -922,6 +922,7 @@ public class AZUnits {
                 width = 10f;
                 drawWidth = 5f;
                 whenShooting = true;
+                region = "flare-shield-shield";
             }});
             weapons.add(new Weapon("az-flare-shield") {{
                 reload = Integer.MAX_VALUE;
