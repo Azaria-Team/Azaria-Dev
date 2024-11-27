1d5749445e79b8fddac64821f67f44e68804ef1d
Slish <90574933+Slish562@users.noreply.github.com>
2024-11-27 19:42:52 +0200

    settings-icon add

--- src/az/ui/dialogs/AzSettings.java
+++ src/az/ui/dialogs/AzSettings.java
@@ -10,17 +10,17 @@ public class AzSettings {
     public static boolean showLightning = true;
 
     public static void load(){
-        ui.settings.addCategory("@settings.azaria","azaria-settings-icon", t -> {
+        ui.settings.addCategory("@settings.azaria","az-settings-icon", t -> {
             t.checkPref("@setting.azaria-show-disclaimer", true);
             t.checkPref("@setting.azaria-show-plasma-lightning", true, value -> {
                 showLightning = value;
             });
         });
     }
-    public static class TableSetting extends Setting {
+    public static class Table extends Setting {
         public Table table;
 
-        public TableSetting(String name, Table table) {
+        public Table(String name, Table table) {
             super(name);
             table = table;
         }
