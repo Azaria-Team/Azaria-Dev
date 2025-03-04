57034f0bd047c66772b3a1664bfb8c3afd966b39
Slish <90574933+Slish562@users.noreply.github.com>
2025-03-04 14:13:39 +0200

    HealingWall liq-heal / add

--- src/az/world/blocks/defense/wall/HealingWall.java
+++ src/az/world/blocks/defense/wall/HealingWall.java
@@ -1,21 +1,46 @@
 package az.world.blocks.defense.wall;
 
+import arc.util.Time;
+import az.content.AZLiquids;
+import az.type.content.AzItem;
+import mindustry.content.Liquids;
+import mindustry.gen.Building;
+import mindustry.type.Liquid;
 import mindustry.world.blocks.defense.Wall;
 
 public class HealingWall extends Wall {
-    public float healAmount = 1f;
+    public float healAmount = 5f;
+    public Liquid healLiquid = AZLiquids.oxyliteLiq;
+    public float liquidConsume = 0.1f;
     public HealingWall(String name) {
         super(name);
         update = true;
-        hasLiquids = false;
+        hasLiquids = true;
+        liquidCapacity = 40;
     }
 
     public class AZHealWallBuild extends WallBuild {
 
         @Override
+        public boolean acceptLiquid(Building source, Liquid liquid) {
+            return liquid == healLiquid;
+        }
+
+        @Override
         public void updateTile() {
             super.updateTile();
-            heal(healAmount * delta());
+
+            if (health < maxHealth && liquids.get(healLiquid) > 0.01f) {
+                // Calculate the required health
+                float healNeeded = maxHealth - health;
+                float healAmounts = healAmount * Time.delta;
+                float maxHeal = Math.min(healAmounts, healNeeded);
+
+                health += maxHeal;
+
+                float consume = liquidConsume * (maxHeal / healAmounts) * Time.delta;
+                liquids.remove(healLiquid, consume);
+            }
         }
     }
 }
